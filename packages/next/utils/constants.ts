b42a55bfc457f06d55caf5a73c0059c49de19076
Entreprenerd <valeranijr@gmail.com>
2021-04-11 18:07:45 +0200

    fix: deploy url for heroku

--- packages/next/utils/constants.ts
+++ packages/next/utils/constants.ts
@@ -2,7 +2,8 @@ import mumbaiDeployment from "@opensky/hardhat/deployments/mumbai/Poster.json";
 
 // TODO MAKE THIS WORK FOR ALL ENVIRONMENTS (or quit and just go with mumbai for hackathon)
 
-export const DEPLOY_BLOG_URL = "https://deploy-ipfs.herokuapp.com";
+export const DEPLOY_BLOG_URL =
+  "https://opensky-deploy-ipfs-docker.herokuapp.com/";
 
 export const EXPLORER_URL = "https://explorer-mumbai.maticvigil.com";
 
