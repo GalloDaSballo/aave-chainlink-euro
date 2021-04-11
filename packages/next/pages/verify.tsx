db4aaa70be5d243c4ae81c2191f5480c016e9657
Entreprenerd <valeranijr@gmail.com>
2021-04-11 18:39:22 +0200

    feat: all steps for chainlink verification

--- packages/next/pages/verify.tsx
+++ packages/next/pages/verify.tsx
@@ -26,6 +26,15 @@ const VerifyAccountPage: React.FC = () => {
   return (
     <div>
       <h2>Verify your Twitter</h2>
+      <p>Step 1: Type your Twitter Handle</p>
+      <p>Step 2: Sign your Twitter Handle</p>
+      <p>Step 3: Publish your Signature on Twitter</p>
+      <p>Step 4: Publish the TweetId containing your signature on Twitter</p>
+      <p>Step 5: Chainlink nodes scans twitter for your Signature</p>
+      <p>
+        Step 6: If the signature matches with your handle and your address,
+        you're verified!
+      </p>
       {signature && <p>{signature}</p>}
       <form onSubmit={handleSignature}>
         <input value={handle} onChange={(e) => setHandle(e.target.value)} />
